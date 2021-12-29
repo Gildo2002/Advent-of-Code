7a70052526275b8c63c5dbf0c25e94d53d1d6ea6
HexLinux <chijin2002@gmail.com>
2021-12-28 23:31:26 -0500

    Day 08 add

---
+++ Day08a.py
@@ -0,0 +1,14 @@
+import numpy as np
+import ast
+from tabulate import tabulate as tb
+
+array = np.loadtxt('Input/Day08.txt',dtype=str,delimiter='\n')
+
+lista = []
+S_tot = M_tot = 0
+
+for i in range(np.shape(array)[0]):
+    S_tot += len(array[i])
+    M_tot += len(ast.literal_eval(array[i]))
+
+print(S_tot - M_tot)
